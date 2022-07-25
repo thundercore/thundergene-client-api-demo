93636b92c8308fa38e39eb917d85502e05b02ce4
waynetseng <wayne.tseng@thundercore.tw>
2022-07-25 15:41:52 +0800

    Add sign/send/nft related API

---
+++ src/create-nft-redeem.js
@@ -0,0 +1,51 @@
+require("dotenv").config();
+
+const utils = require("./utils");
+const fetch = require("node-fetch");
+
+const { ENDPOINT, TARGET_ADDRESS, ASSET_ID, ASSET_ID2 } = process.env;
+
+async function main() {
+  const body = {
+    from: TARGET_ADDRESS,
+    redeem_assets: [
+      {
+        asset_id: ASSET_ID,
+        balance: [
+          {
+            token_id: "1",
+            amount: 1,
+          },
+        ],
+      },
+      {
+        asset_id: ASSET_ID2,
+        balance: [
+          {
+            token_id: "1",
+            amount: 1,
+          },
+        ],
+      },
+    ],
+  };
+
+  const stringifyBody = JSON.stringify(body);
+  const response = await fetch(`${ENDPOINT}/api/v1/client/redeem/create/nft`, {
+    method: "post",
+    headers: utils.genHeader(stringifyBody),
+    body: stringifyBody,
+  });
+
+  const data = await response.json();
+
+  await utils.waitRedeemTask(data.id);
+
+  const status = await utils.getRedeemStatus(data.id);
+
+  console.log(
+    `Create redeem done, get redeem code: ${status.redeem_code}, status: ${status.status}`
+  );
+}
+
+main().catch((e) => console.log(e));
--- src/user-balance.js
+++ src/user-balance.js
@@ -7,7 +7,7 @@ const { ENDPOINT, TARGET_ADDRESS, ASSET_ID } = process.env;
 
 async function main() {
   const response = await fetch(
-    `${ENDPOINT}/api/v1/client/user/nft?asset_id=${ASSET_ID}&address=${TARGET_ADDRESS}`,
+    `${ENDPOINT}/api/v1/client/user?asset_id=${ASSET_ID}&address=${TARGET_ADDRESS}`,
     {
       method: "get",
       headers: utils.genHeader(""),
@@ -15,7 +15,7 @@ async function main() {
   );
 
   const data = await response.json();
-  console.log(`User balance of asset: ${ASSET_ID} = ${data.balance}`);
+  console.log(`User balance of asset: ${ASSET_ID} = ${JSON.stringify(data)}`);
 }
 
 main().catch((e) => console.log(e));
